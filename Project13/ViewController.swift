5119ed4e01847cd680d9d72f1474f496a137f06e
Dan O'Leary <impulsecoupled@gmail.com>
2019-03-26 16:46:05 -0500

    Day 52: Project 13 of 100DaysOfSwift

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    var currentImage: UIImage!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "YACIFP"
     }
-
+    
+    @IBAction func importPicture(_ sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true, completion: nil)
+        
+        currentImage = image
+    }
 
 }
 
